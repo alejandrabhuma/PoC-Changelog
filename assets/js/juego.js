7ac33a3f4ee2c2ef76e57a9cabf64e0f5d864230
Alejandra Merino <merino.ale97@estudiantec.cr>
2023-08-09 11:44:23 -0600

    Add game context

---
+++ assets/js/juego.js
@@ -0,0 +1,20 @@
+(() => {
+    'use strict' //Se recomienda usar en todo momento que se tabaje con patrón módulo
+
+
+    let deck = [];
+    const tipos = ['C', 'D', 'H', 'S'],
+        especiales = ['A', 'J', 'Q', 'K'];
+    let puntosJugadores = [];
+
+
+//Referencias del HTML
+    const btnPedir = document.querySelector('#btnPedir'),
+        btnDetener = document.querySelector('#btnDetener'),
+        btnNuevo = document.querySelector('#btnNuevo');
+
+    const puntosHTML = document.querySelectorAll('small'),
+        divCartasJugador = document.querySelector('#jugador-cartas'),
+        divCartasComputadora = document.querySelector('#computadora-cartas');
+    
+})();
